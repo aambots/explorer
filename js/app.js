f0041d62b886f3ebd79e9f4bf8a93551d5d8fb1e
Melvin Carvalho <https://melvincarvalho.com/#me>
2021-11-22 14:20:28 +0100

    improve tx view

--- js/app.js
+++ js/app.js
@@ -5,16 +5,44 @@ console.log(99)
 
 console.log(di.data)
 
-render(
-  html`
-    <h1>Hello Spux!</h1>
-  `,
-  document.body
-)
+// di.data.txid = `<a href="${di.data.txid}">${di.data.txid}</a>`
 
 render(html`
 <h1>Tx ${di.data.txid}</h1>
 <pre>${JSON.stringify(di.data, null, 2)}</pre>
+
+
+
+<a href="${di.data.vin[0].txid}">prev</a><br/>
+<a href="${di.data.txid}.json">json</a><br/>
+`, document.body)
+
+var d = di.data
+
+render(html`
+<h1>Tx ${di.data.txid}</h1>
+
+Outputs
+${d.vout.map(i => html`<pre>Address: ${i.scriptPubKey?.addresses[0]} 
+${'\n'}
+Amount: ${i.value}
+${'\n'}
+Pub Key: ${i.scriptPubKey?.hex}
+</pre>`)}
+
+Inputs
+${d.vin.map(i => html`<pre>Txn: <a href=${i.txid}>${i.txid}:${i.vout}</a> 
+${'\n'}
+Hex: ${i.scriptSig?.hex}
+</pre>`)}
+
+Raw Data
+
+<pre> ${JSON.stringify(d.vin, null, 2)}</pre>
+
+<pre>${JSON.stringify(d.vout, null, 2)}</pre>
+
+
 
 <a href="${di.data.vin[0].txid}">prev</a><br/>
 <a href="${di.data.txid}.json">json</a><br/>
